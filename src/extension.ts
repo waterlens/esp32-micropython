184af849cf36e764ba50e13c2fc2844179df76ce
Xingkai Wang <bittervanzju@gmail.com>
2022-03-12 18:58:43 +0800

    finish serial terminal demo

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,14 @@
 import * as vscode from "vscode";
 import { DownloadUtil } from "./downloadUtil";
 import { PortProvider } from "./portView";
-import { launchTerminal, showAvailablePorts } from "./terminal";
+import { TerminalWrapper, pickPort } from "./terminal";
+// import { CustomBuildTaskProvider } from "./terminal";
+
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('"esp32-micropython" is now active!');
+  // This map is used to hold map<port-path, port> 
+  let portMap = new Map();
 
   const portView = new PortProvider();
   const downloadUtil = new DownloadUtil();
@@ -21,9 +25,18 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
+  // context.subscriptions.push(
+  //   vscode.commands.registerCommand("emp.terminal.connect", (portPath: string) => {
+  //     // new TerminalWrapper(portMap, portPath)
+  //   })
+  // );
+
   context.subscriptions.push(
-    vscode.commands.registerCommand("emp.terminal.launch", () =>
-      launchTerminal()
+   vscode.commands.registerCommand("emp.terminal.launch", async () => 
+      {
+        pickPort(portMap);
+        console.log(portMap);
+      }
     )
   );
 
--- src/terminal.ts
+++ src/terminal.ts
@@ -1,6 +1,7 @@
 import { SerialPort } from "serialport";
 import { ReadlineParser } from "@serialport/parser-readline";
 import { SerialPortStream } from "@serialport/stream";
+import { ByteLengthParser } from "serialport";
 import {
   SetOptions,
   BindingInterface,
@@ -8,28 +9,133 @@ import {
   OpenOptionsFromBinding,
 } from "@serialport/bindings-interface";
 import { TestRunRequest, window } from "vscode";
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { maxHeaderSize } from "http";
 
 export function showAvailablePorts() {
   const portList = SerialPort.list();
   portList.then((ports) => console.log(ports));
 }
 
-export function launchTerminal() {
-  showAvailablePorts();
-  const parser = new ReadlineParser({ delimiter: "\r\n" });
-  const port = new SerialPort({
-    path: "COM4",
-    baudRate: 115200,
-    hupcl: false,
-  });
-  let output = window.createOutputChannel("ESP Terminal");
-  port.pipe(parser);
-  parser.on("error", (err) => {
-    output.appendLine("Error: " + err);
-  });
-  parser.on("open", () => output.appendLine("Port opened"));
-  parser.on("data", (data) => output.appendLine(data.toString()));
-  port.write("help()\r\n");
-  port.write("import machine\r\n");
-  port.write("machine.soft_reset()\r\n");
+export class EmpTerminal implements vscode.Pseudoterminal {
+	private writeEmitter = new vscode.EventEmitter<string>();
+	onDidWrite: vscode.Event<string> = this.writeEmitter.event;
+
+	
+
+	private closeEmitter = new vscode.EventEmitter<number>();
+	onDidClose?: vscode.Event<number> = this.closeEmitter.event;
+
+	private port;
+	private parser;
+	private portMap;
+
+	handleInput(data: string): void {
+		this.port?.write(data);
+	}
+
+	constructor(portMap: Map<string, SerialPort>, portPath: string) {
+		this.portMap = portMap;
+
+		this.port = portMap.get(portPath);
+		// this.parser = new ReadlineParser({ delimiter: "\r\n" });
+		this.parser = new ByteLengthParser({length: 1});
+		this.port?.pipe(this.parser);
+
+
+		this.parser.on("error", (err) => {
+			this.writeEmitter.fire("Error: " + err.toString() + "\r\n");
+		});
+
+		this.parser.on("open", () => {
+			this.writeEmitter.fire("Serial Connected: " + portPath + "\r\n");
+		});
+
+		this.parser.on("data", (data) => {
+			this.writeEmitter.fire(data.toString());
+		});
+	}
+
+	open(initialDimensions: vscode.TerminalDimensions | undefined): void {
+
+
+		const portList = SerialPort.list();
+		let pathName = "";
+		portList.then((ports) => {
+			if (ports.length > 1) {
+				this.writeEmitter.fire("Please select");
+				for (let port of ports) {
+					this.writeEmitter.fire(port.path);
+				}
+			} else {
+
+			}
+		});
+		console.log('it is open');
+	}
+
+	close(): void {
+
+	}
+}
+
+let terminalMap = new Map();
+
+export class TerminalWrapper {
+	terminal;
+	portPath;
+
+	constructor(portMap: Map<string, SerialPort>, portPath: string) {
+		this.portPath = portPath;
+		if (!terminalMap.has(portPath)) {
+			this.terminal = (<any>vscode.window).createTerminal({
+				name: 'Serial: ' + portPath,
+				pty: new EmpTerminal(portMap, portPath),
+			});
+			terminalMap.set(portPath, this.terminal);
+		} else {
+			this.terminal = terminalMap.get(portPath);
+		}
+
+	}
+
+	show() {
+		this.terminal.show();
+	}
+}
+
+class PortPickItem implements vscode.QuickPickItem{
+	label: string;
+	constructor(name: string) {
+		this.label = name;
+	}
+}
+
+
+// Pop up a quick pick window, let user to choose a serial port path
+// It the port is not connected yet, add the port to the path
+export function pickPort(portMap: Map<string, SerialPort>) {
+	const quickPick = vscode.window.createQuickPick();
+	const portList = SerialPort.list();
+	portList.then((ports) => {
+		quickPick.items = ports.map(port => new PortPickItem(port.path));
+	});
+	quickPick.onDidHide(() => quickPick.dispose());
+	quickPick.onDidChangeSelection(
+		selection => {
+			let portPath = selection[0].label;
+			quickPick.dispose();
+			if (!portMap.has(portPath)) {
+				portMap.set(portPath, new SerialPort({
+					path: portPath,
+					baudRate: 115200,
+					hupcl: false,
+				}));
+			}
+			new TerminalWrapper(portMap, portPath).show();
+
+		}
+	);
+	quickPick.show();
 }
